c8c9007c8e5d449419d86d2b88d14b8ac9cc56f2
saddamhr <saddambubt65@gmail.com>
2020-03-22 19:16:13 +0600

    integrate auth, custom hook, guard shipment route

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Cart = (props) => {
     const cart = props.cart;
+    // const user = useContext(UserContext);
     // const totalPrice = cart.reduce((total, prd) => total + prd.price, 0);
     let total = 0;
     for(let i = 0; i < cart.length; i++) {
@@ -19,7 +20,7 @@ const Cart = (props) => {
         shipping = 12.99;
     }
     const tax = (total / 10).toFixed(2)
-    const grandTotal = (total + shipping +Number(tax).toFixed(2))
+    const grandTotal = (total + shipping +Number(tax)).toFixed(2)
 
     const formatNumber = num => {
         const precision = num.toFixed(2);
@@ -32,7 +33,7 @@ const Cart = (props) => {
             <p>Product Price: {formatNumber(total)}</p>
             <p><small>Shipping Cost: {shipping}</small></p>
             <p><small>Tax + VAT: {tax}</small></p>
-            <p>Total Price: {}</p>
+            <p>Total Price: {grandTotal}</p>
             <br/>
             {
                 props.children
