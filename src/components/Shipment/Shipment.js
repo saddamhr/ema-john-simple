ae93d18b9265b91a4f077b4e4428ba12bd79e190
saddamhr <saddambubt65@gmail.com>
2020-04-05 09:56:51 +0600

    ---

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -2,16 +2,36 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import './Shipment.css'
 import { useAuth } from '../Login/useAuth';
-import { getDatabaseCart, processOrder } from '../../utilities/databaseManager';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import { getDatabaseCart, clearLocalShoppingCart } from '../../utilities/databaseManager';
+import CheckoutForm from '../CheckoutForm/CheckoutForm';
+import { useState } from 'react';
 
 const Shipment = () => {
     const { register, handleSubmit, errors } = useForm();
+    const [shipInfo, setShipInfo] = useState(null);
+    const [orderId, setOrderId] = useState(null);
+
     const auth = useAuth();
+
+    const stripePromise = loadStripe('pk_test_SfI6il7qsnTOwDdG9413uNaa005qk9Mryl');
+
     const onSubmit = data => {
+        setShipInfo(data);
+
+    }
+
+    const handlePlaceOrder = (payment) => {
         // TODO: Saddam move this after payment
-        
+
         const savedCart = getDatabaseCart();
-        const orderDetails = {email: auth.user.email, cart:savedCart};
+        const orderDetails = {
+            email: auth.user.email,
+            cart: savedCart,
+            shipment: shipInfo,
+            paymentMethod: payment
+        };
         fetch('http://localhost:4200/placeOrder', {
             method: 'POST',
             headers: {
@@ -19,50 +39,71 @@ const Shipment = () => {
             },
             body: JSON.stringify(orderDetails)
         })
-        .then(res => res.json())
-        .then(data => {
-            alert('Successfully placed your order with order id:' +data._id)
-            processOrder();
-        })
-        
+            .then(res => res.json())
+            .then(order => {
+                setOrderId(order._id);
+                clearLocalShoppingCart();
+                // clear local storage cart
+                // give thanks to the user
+            })
     }
 
     return (
-        <form className='ship-form' onSubmit={handleSubmit(onSubmit)}>
-            <input name="name" defaultValue={auth.user.name} ref={register({ required: true })} placeholder='Your name' />
-            {
-                errors.name && <span className='error'>Name is required</span>
-            }
+        <div className="container">
+            <div className="row">
+                <div style={{ display: shipInfo && 'none' }} className="col-md-6">
+                    <h3>Shipping Information</h3>
+                    <form className='ship-form' onSubmit={handleSubmit(onSubmit)}>
+                        <input name="name" defaultValue={auth.user.name} ref={register({ required: true })} placeholder='Your name' />
+                        {
+                            errors.name && <span className='error'>Name is required</span>
+                        }
 
-            <input name="email" defaultValue={auth.user.email} ref={register({ required: true })} placeholder='Your email' />
-            {
-                errors.email && <span className='error'>Email is required</span>
-            }
+                        <input name="email" defaultValue={auth.user.email} ref={register({ required: true })} placeholder='Your email' />
+                        {
+                            errors.email && <span className='error'>Email is required</span>
+                        }
 
-            <input name="AddressLine1" ref={register({ required: true })} placeholder='Address Line 1' />
-            {
-                errors.AddressLine1 && <span className='error'>Address is required</span>
-            }
+                        <input name="AddressLine1" ref={register({ required: true })} placeholder='Address Line 1' />
+                        {
+                            errors.AddressLine1 && <span className='error'>Address is required</span>
+                        }
 
-            <input name="AddressLine2" ref={register} placeholder='Address Line 2' />
+                        <input name="AddressLine2" ref={register} placeholder='Address Line 2' />
 
-            <input name="city" ref={register({ required: true })} placeholder='City' />
-            {
-                errors.city && <span className='error'>City is required</span>
-            }
+                        <input name="city" ref={register({ required: true })} placeholder='City' />
+                        {
+                            errors.city && <span className='error'>City is required</span>
+                        }
 
-            <input name="country" ref={register({ required: true })} placeholder='Country' />
-            {
-                errors.country && <span className='error'>Country is required</span>
-            }
+                        <input name="country" ref={register({ required: true })} placeholder='Country' />
+                        {
+                            errors.country && <span className='error'>Country is required</span>
+                        }
 
-            <input name="zipcode" ref={register({ required: true })} placeholder='Zip Code' />
-            {
-                errors.zipcode && <span className='error'>Zipcode is required</span>
-            }
-            
-            <input type="submit" />
-        </form>
+                        <input name="zipcode" ref={register({ required: true })} placeholder='Zip Code' />
+                        {
+                            errors.zipcode && <span className='error'>Zipcode is required</span>
+                        }
+
+                        <input type="submit" />
+                    </form>
+                </div>
+                <div style={{ display: shipInfo ? 'block' : 'none' }} className="col-md-6">
+                    <h4>Payment Information</h4>
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm handlePlaceOrder={handlePlaceOrder}></CheckoutForm>
+                    </Elements>
+                    <br/>
+                    {
+                        orderId && <div>
+                            <h3>Thank you for shopping with us</h3>
+                            <p>Your order id is: {orderId}</p>
+                        </div>
+                    }
+                </div>
+            </div>
+        </div>
     )
 };
 
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,72 +1,75 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseManager';
 import { Link } from 'react-router-dom';
-import { addToDatabaseCart, getDatabaseCart} from '../../utilities/databaseManager'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
 
-    useEffect(() => {
+    useEffect(()=>{
         fetch('http://localhost:4200/products')
         .then(res => res.json())
         .then(data => {
-            setProducts(data)
+            setProducts(data);
         })
     }, [])
-
-    useEffect(() => {
+    
+    useEffect(()=>{
         const savedCart = getDatabaseCart();
         const productKeys = Object.keys(savedCart);
-        if(products.length) {
+        if(products.length > 0){
             const previousCart = productKeys.map( existingKey => {
-                const product = products.find( pd => pd.key === existingKey)
+                const product = products.find( pd => pd.key === existingKey);
                 product.quantity = savedCart[existingKey];
                 return product;
-            })
+            } )
             setCart(previousCart);
         }
-    },[products])
+    }, [products])
 
-    const handleAddProduct = (product) => {
+    const handleAddProduct = (product) =>{
         const toBeAddedKey = product.key;
-        const sameProduct = cart.find(pd => pd.key === toBeAddedKey)
+        const sameProduct = cart.find(pd => pd.key === toBeAddedKey);
         let count = 1;
         let newCart;
-        if(sameProduct) {
+        if(sameProduct){
             count = sameProduct.quantity + 1;
             sameProduct.quantity = count;
-            const others = cart.filter(pd => pd.key !== toBeAddedKey)
+            const others = cart.filter(pd => pd.key !== toBeAddedKey);
             newCart = [...others, sameProduct];
-        } else {
-            product.quantity= 1
+        }
+        else{
+            product.quantity = 1;
             newCart = [...cart, product];
         }
         setCart(newCart);
-        addToDatabaseCart(product.key, count)
-
+        addToDatabaseCart(product.key, count);
     }
+
     return (
-        <div className='twin-container'>
+        <div className="twin-container">
             <div className="product-container">
                 {
-                    products.map(pd => <Product
-                    key = {pd.key}
-                    showAddToCart = {true}
+                    products.map(pd => <Product 
+                        key={pd.key}
+                        showAddToCart={true}
                         handleAddProduct = {handleAddProduct}
                         product={pd}
-                        >{pd.name}</Product>)
+                        ></Product>)
                 }
             </div>
             <div className="cart-container">
-                <Cart cart={cart}>
-                    <Link to='/review'>
-                        <button className='main-button'>Review Order</button>
+               <Cart cart={cart}>
+                    <Link to="/review">
+                        <button className="main-button">Review Order</button>
                     </Link>
-                </Cart>
+               </Cart>
             </div>
+            
         </div>
     );
 };
